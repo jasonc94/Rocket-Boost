7a42d41505e71a141fdeb5392276c4704d7e0efc
Jason Cai <jasoncai528@gmail.com>
2018-12-05 19:07:18 -0700

    rocket object created

---
+++ Assets/Rocket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RocketMovementInput();
+	}
+
+     void RocketMovementInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+        
+        }
+    }
+}
